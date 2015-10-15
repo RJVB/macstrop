727e092207eb80fff4d0fef8334a1325b1a0e40b
René J.V. Bertin <rjvbertin@gmail.com>
2015-10-15 17:57:39 +0200

    port:calligra-translations: bump to v2.9.8

--- kde/calligra-l10n-translations/files/update-checksum-tables.sh
+++ kde/calligra-l10n-translations/files/update-checksum-tables.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
 
-for J in "$@" ;do
-	echo "{`openssl rmd160 $J | sed -e 's/.*)= //g'` `echo $J | sed -e 's/.*calligra-l10n-\(.*\)-2.9.2.tar.xz/\1/g'`}"
+VERSION="`port info --version calligra-translations | sed -e 's|version: ||g'`"
+echo "Calligra version: ${VERSION}"
+PFILE="`port dir calligra-translations`/Portfile"
+LANGS="`fgrep 'set languages' ${PFILE} | sed -e 's|set languages.*{\(.*\)}|\1|g'`"
+FILES=""
+
+set -e
+echo "Fetching distfiles ..."
+for J in ${LANGS} ;do
+    FILE="`port distfiles calligra-l10n-$J | grep '^\[' | sed -e 's|.*\] ||g'`"
+    if [ ! -r ${FILE} ] ;then
+        port fetch calligra-l10n-$J
+        echo "Fetched distfile ${FILE} for language $J"
+    fi
+    FILES="${FILES} ${FILE}"
 done
 
-for J in "$@" ;do
-	echo "{`openssl sha256 $J | sed -e 's/.*)= //g'` `echo $J | sed -e 's/.*calligra-l10n-\(.*\)-2.9.2.tar.xz/\1/g'`}"
+echo "set rmd160s     {"
+for J in $FILES ;do
+    LANG=`echo $J | sed -e "s|.*calligra-l10n-\(.*\)-${VERSION}.tar.xz|\1|g"`
+    echo "                    {`openssl rmd160 $J | sed -e 's/.*)= //g'` ${LANG}}"
 done
+echo "                }"
+
+echo "set sha256s     {"
+for J in $FILES ;do
+    LANG=`echo $J | sed -e "s/.*calligra-l10n-\(.*\)-${VERSION}.tar.xz/\1/g"`
+    echo "                    {`openssl sha256 $J | sed -e 's/.*)= //g'` ${LANG}}"
+done
+echo "                }"
