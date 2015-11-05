ac0d4913ad75cb179c796df317e833108b524d49
René J.V. Bertin <rjvbertin@gmail.com>
2015-11-05 17:38:56 +0100

    KF5 PortGroup : kiconthemes dep

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -252,6 +252,7 @@ kf5.framework_dependency    kguiaddons libKF5GuiAddons
 kf5.framework_dependency    kwidgetsaddons libKF5WidgetsAddons
 kf5.framework_dependency    kconfigwidgets libKF5ConfigWidgets
 kf5.framework_dependency    kitemviews libKF5ItemViews
+kf5.framework_dependency    kiconthemes libKF5IconThemes
 
 #########
 # to install kf5-frameworkintegration:
