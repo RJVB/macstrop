01f0e7186db7578199d0b7f46a3ce336a5a47770
René J.V. Bertin <rjvbertin@gmail.com>
2015-11-21 20:28:06 +0100

    KF5 PortFile: baloo dep

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -383,6 +383,7 @@ kf5.framework_dependency    kxmlgui libKF5XmlGui
 kf5.framework_dependency    kbookmarks libKF5Bookmarks
 kf5.framework_dependency    kwallet libKF5Wallet
 kf5.framework_dependency    kio libKF5KIOCore
+kf5.framework_dependency    baloo libKF5Baloo
 
 #########
 # to install kf5-frameworkintegration:
