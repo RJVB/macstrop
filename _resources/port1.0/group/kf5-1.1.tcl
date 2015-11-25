b5c212c8e55c90421d22ae849f62a996b862b28f
René J.V. Bertin <rjvbertin@gmail.com>
2015-11-25 23:04:00 +0100

    KF5 PortFile: kdesignerplugin dep.

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -400,6 +400,7 @@ kf5.framework_dependency    kinit libkdeinit5_klauncher
 kf5.framework_dependency    kded libkdeinit5_kded5
 kf5.framework_dependency    kparts libKF5Parts
 kf5.framework_dependency    kdewebkit libKF5WebKit
+set kf5.kdesignerplugin_dep path:bin/kgendesignerplugin:kf5-kdesignerplugin
 
 # see also http://api.kde.org/frameworks-api/frameworks5-apidocs/attica/html/index.html
 
