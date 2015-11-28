59ed1c9faa263fcee690d60e698a6bbcc5fba8ef
René J.V. Bertin <rjvbertin@gmail.com>
2015-11-28 11:24:27 +0100

    KF5 PortGroup : frameworkintegration dep

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -444,6 +444,7 @@ kf5.framework_dependency    kdewebkit libKF5WebKit
 set kf5.kdesignerplugin_dep path:bin/kgendesignerplugin:kf5-kdesignerplugin
 kf5.framework_dependency    kpty libKF5Pty
 kf5.framework_dependency    kdelibs4support libKF5KDELibs4Support
+kf5.framework_dependency    frameworkintegration libKF5Style
 
 # see also http://api.kde.org/frameworks-api/frameworks5-apidocs/attica/html/index.html
 
