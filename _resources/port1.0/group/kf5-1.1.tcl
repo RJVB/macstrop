edf04def2d6ee3e92d30c7954643b071cdf8284a
René J.V. Bertin <rjvbertin@gmail.com>
2015-12-02 23:07:28 +0100

    KF5 PortGroup : kpeople dep

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -445,6 +445,7 @@ set kf5.kdesignerplugin_dep path:bin/kgendesignerplugin:kf5-kdesignerplugin
 kf5.framework_dependency    kpty libKF5Pty
 kf5.framework_dependency    kdelibs4support libKF5KDELibs4Support
 kf5.framework_dependency    frameworkintegration libKF5Style
+kf5.framework_dependency    kpeople libKF5People
 
 # see also http://api.kde.org/frameworks-api/frameworks5-apidocs/attica/html/index.html
 
