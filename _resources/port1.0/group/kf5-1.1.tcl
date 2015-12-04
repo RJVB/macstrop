9282c99f458978cbff7eae3ce1e0356ac0a059a4
René J.V. Bertin <rjvbertin@gmail.com>
2015-12-04 23:01:13 +0100

    KF5 PortGroup: kactivities dep
    Committed from host : Portia.local

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -448,6 +448,7 @@ kf5.framework_dependency    frameworkintegration libKF5Style
 kf5.framework_dependency    kpeople libKF5People
 kf5.framework_dependency    ktexteditor libKF5TextEditor
 kf5.framework_dependency    kxmlrcpclient libKF5XmlRpcClient
+kf5.framework_dependency    kactivities libKF5Activities
 
 # see also http://api.kde.org/frameworks-api/frameworks5-apidocs/attica/html/index.html
 
