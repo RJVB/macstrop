375db4b2f9fde798c7b3acd4373ebacd1d84f71e
René J.V. Bertin <rjvbertin@gmail.com>
2015-12-04 22:14:37 +0100

    KF5 PortGroup: kxmlrpcclient dep
    Committed from host : Portia.local

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -447,6 +447,7 @@ kf5.framework_dependency    kdelibs4support libKF5KDELibs4Support
 kf5.framework_dependency    frameworkintegration libKF5Style
 kf5.framework_dependency    kpeople libKF5People
 kf5.framework_dependency    ktexteditor libKF5TextEditor
+kf5.framework_dependency    kxmlrcpclient libKF5XmlRpcClient
 
 # see also http://api.kde.org/frameworks-api/frameworks5-apidocs/attica/html/index.html
 
