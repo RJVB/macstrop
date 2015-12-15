0b5a7c5b636b5676801f7300be269922cc1cd8cb
René J.V. Bertin <rjvbertin@gmail.com>
2015-12-15 12:18:00 +0100

    Qt5-kde PortGroup : fix typo
    Committed from host : patux

--- _resources/port1.0/group/qt5-kde-1.0.tcl
+++ _resources/port1.0/group/qt5-kde-1.0.tcl
@@ -294,7 +294,7 @@ if {![info exists building_qt5]} {
         set qt5_dependency ${qt5_pathlibspec}:qt5-kde
         depends_lib-append ${qt5_dependency}
     } elseif {${os.platform} eq "linux"} {
-        set qt5_pathlibspec path:libexec/${qt_name}/lib/libQtCore.${qt_libs_ext}
+        set qt5_pathlibspec path:libexec/${qt_name}/lib/libQt5Core.${qt_libs_ext}
         set qt5_dependency ${qt5_pathlibspec}:qt5-kde
         depends_lib-append ${qt5_dependency}
     }
