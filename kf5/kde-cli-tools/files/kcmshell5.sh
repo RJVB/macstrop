02d67f50a400020632e46c76bee0c8a88f7a3dbb
René J.V. Bertin <rjvbertin@gmail.com>
2015-12-22 21:29:39 +0100

    port:kf5-cli-tools : build kcmshell5 as an app bundle and install a wrapper script that starts it through LaunchServices
    Committed from host : Portia.local

---
+++ kf5/kde-cli-tools/files/kcmshell5.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# cache current KDE_SESSION_VERSION setting
+KSV="`launchctl getenv KDE_SESSION_VERSION`"
+if [ "${KDE_SESSION_VERSION}" != "" ] ;then
+    launchctl setenv KDE_SESSION_VERSION "${KDE_SESSION_VERSION}"
+else
+    # KDE_SESSION_VERSION not set; unset it for LaunchServices too.
+    # Note that this doesn't appear to be effective for the kcmshell5.app
+    # instance we'll be launching.
+    launchctl unsetenv KDE_SESSION_VERSION
+fi
+
+open -W -n -a @KDEAPPDIR@/kcmshell5.app --args "$@" &
+PID=$!
+
+# restore what we changed
+if [ "${KSV}" != "" ] ;then
+    launchctl setenv KDE_SESSION_VERSION "${KSV}"
+else
+    launchctl unsetenv KDE_SESSION_VERSION
+fi
+
+# wait for kcmshell5 to exit and raise its return code
+wait ${PID}
+exit $?
