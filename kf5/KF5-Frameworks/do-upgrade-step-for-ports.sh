41b747d75c5169717aa1dbc0bb790069821e4021
René J.V. Bertin <rjvbertin@gmail.com>
2016-02-15 15:39:49 +0100

    ATTN: WIP! port:KF5-Frameworks : bumping to v 5.19.0 ; Tier3 frameworks done. port:kf5-kcoreaddons : adds QspXDG activator to desktoptojson as that is now required
    Committed from host : Portia.local

--- kf5/KF5-Frameworks/do-upgrade-step-for-ports.sh
+++ kf5/KF5-Frameworks/do-upgrade-step-for-ports.sh
@@ -5,10 +5,18 @@ STEP="$2"
 COPTS="$3"
 shift 3
 
+FAILS=""
 for J in "$@" ;do
     if [ "${COPTS}" != "" ] ;then
         port ${POPTS} ${STEP} `port-active-variants -echo ${J}` configure.optflags="${COPTS}"
     else
         port ${POPTS} ${STEP} `port-active-variants -echo ${J}`
     fi
+    if [ $? != 0 ] ;then
+        FAILS="${FAILS} ${J}"
+    fi
 done
+
+if [ "${FAILS}" != "" ] ;then
+    echo "Failures for step \"${STEP}\": ${FAILS}"
+fi
