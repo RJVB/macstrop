397a5efc8bf172705f6b61ddc52ef49a997ec536
René J.V. Bertin <rjvbertin@gmail.com>
2016-06-15 21:27:48 +0200

    KF5 PortGroup : Applications latest upgraded to 16.04.2
    Committed from host : Portia.local

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -85,7 +85,7 @@ if {![info exists kf5.version]} {
 if {![ info exists kf5.release ]} {
     set kf5.release     16.04.0
     set kf5.latest_release \
-                        16.04.1
+                        16.04.2
 }
 
 # KF5 Plasma version
