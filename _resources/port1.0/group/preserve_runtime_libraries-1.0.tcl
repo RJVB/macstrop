62afc784dc31c0080286fa132f8a45814cefffc7
René J.V. Bertin <rjvbertin@gmail.com>
2016-06-22 18:54:09 +0200

    preserve_runtime_libraries PortGroup initial commit
    Committed from host : Portia.local

---
+++ _resources/port1.0/group/preserve_runtime_libraries-1.0.tcl
@@ -0,0 +1,63 @@
+# -*- coding: utf-8; mode: tcl; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; truncate-lines: t -*- vim:fenc=utf-8:et:sw=4:ts=4:sts=4
+# $Id:$
+
+# Copyright (c) 2015 The MacPorts Project
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Apple Computer, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Usage:
+# PortGroup     preserve_runtime_libraries 1.0
+
+# set libraries_to_preserve ""
+proc preserve_libraries {libs} {
+    upvar #0 libraries_to_preserve liblist
+    foreach l ${libs} {
+        if {[info exists liblist]} {
+            set liblist "${liblist} ${l}"
+        } else {
+            set liblist ${l}
+        }
+    }
+}
+
+variant preserve_runtime_libraries description {Experimental variant that preserves the pre-existing runtime \
+                                        libraries to ease the rebuilding load during upgrades. Note that\
+                                        ports that depend on poppler via poppler-qt4-mac or poppler-qt5-mac \
+                                        do not need this trick to avoid rebuilding.} {
+    post-destroot {
+        foreach l ${libraries_to_preserve} {
+            if {![file exists ${destroot}${l}]} {
+                ui_info "Preserving previous runtime shared library ${l}"
+                copy ${l} ${destroot}${l}
+            }
+        }
+    }
+}
+
+# kate: backspace-indents true; indent-pasted-text true; indent-width 4; keep-extra-spaces true; remove-trailing-spaces modified; replace-tabs true; replace-tabs-save true; syntax Tcl/Tk; tab-indents true; tab-width 4;
+
