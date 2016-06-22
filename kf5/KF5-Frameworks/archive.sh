533eb4219771ceb92d1401f512143e6e72b0d070
René J.V. Bertin <rjvbertin@gmail.com>
2016-06-22 22:10:15 +0200

    port:KF5-Frameworks: a bit of cleanup
    Committed from host : Portia.local

--- kf5/KF5-Frameworks/archive.sh
+++ kf5/KF5-Frameworks/archive.sh
@@ -9,7 +9,7 @@ CATDIR=`basename ${PWD}`
 cd ..
 
 EXTRAFILES="_resources/port1.0/group/kf5-1.1.tcl _resources/port1.0/group/locale_select-1.0.tcl _resources/port1.0/group/cmake-1.0.tcl _resources/port1.0/group/code-sign-1.0.tcl"
-EXTRAFILES="${EXTRAFILES} kf5/kf5-osx-integration kde/QtCurve"
+EXTRAFILES="${EXTRAFILES} kf5/kf5-gpgmepp kf5/kf5-osx-integration kde/QtCurve"
 
 # echo ${PORT} ${CATDIR} ${PORTDIR}
 tar -c  --exclude "*.kdev4" --exclude ".DS_Store" --exclude="*.orig" -vjf ${PORT}.tar.bz2 ${EXTRAFILES} ${CATDIR}/${PORTDIR}
