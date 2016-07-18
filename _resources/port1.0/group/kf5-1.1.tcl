0fc3c80213e63c85d81a75a76a99606de172e59b
René J.V. Bertin <rjvbertin@gmail.com>
2016-07-18 21:41:40 +0200

    KF5 PortGroup : adapt the apidocs generation to kf5-kapidox 5.24.0 :-/
    Committed from host : Bola

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -245,13 +245,26 @@ variant docs description {build and install the API documentation, for use with 
                 ui_msg "--->  Generating documentation for ${subport}"
                 # generate the documentation, working from ${build.dir}
                 xinstall -m 755 -d ${destroot}${kf5.docs_dir}
-                system -W ${build.dir} "kgenapidox --qhp --searchengine --api-searchbox \
-                    --qtdoc-dir ${qt_docs_dir} --kdedoc-dir ${kf5.docs_dir} \
-                    --qhelpgenerator ${qt_bins_dir}/qhelpgenerator ${worksrcpath}"
-                # after creating the destination, copy all generated qch documentation to it
-                foreach doc [glob -nocomplain ${build.dir}/apidocs/qch/*.qch] {
-                    if {${doc} ne "${build.dir}/apidocs/qch/None.qch"} {
-                        xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
+                # this appears to be necessary, sometimes:
+                system "chmod 755 ${destroot}${kf5.docs_dir}"
+                if {[file exists ${prefix}/bin/kgenapidox]} {
+                    system -W ${build.dir} "kgenapidox --qhp --searchengine --api-searchbox \
+                        --qtdoc-dir ${qt_docs_dir} --kdedoc-dir ${kf5.docs_dir} \
+                        --qhelpgenerator ${qt_bins_dir}/qhelpgenerator ${worksrcpath}"
+                    # after creating the destination, copy all generated qch documentation to it
+                    foreach doc [glob -nocomplain ${build.dir}/apidocs/qch/*.qch] {
+                        if {${doc} ne "${build.dir}/apidocs/qch/None.qch"} {
+                            xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
+                        }
+                    }
+                } else {
+                    system -W ${build.dir} "kapidox_generate --qhp --searchengine --api-searchbox \
+                        --qtdoc-dir ${qt_docs_dir} --qhelpgenerator ${qt_bins_dir}/qhelpgenerator ${worksrcpath}"
+                    # after creating the destination, copy all generated qch documentation to it
+                    foreach doc [glob -nocomplain ${build.dir}/frameworks/*/qch/*.qch] {
+                        if {[file tail ${doc}] ne "None.qch"} {
+                            xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
+                        }
                     }
                 }
                 # cleanup
