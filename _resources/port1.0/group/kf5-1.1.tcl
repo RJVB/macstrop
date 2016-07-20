d34909fd7664dc483534608e15d15a06ef08a642
René J.V. Bertin <rjvbertin@gmail.com>
2016-07-20 09:02:39 +0200

    KF5 PortGroup: upgrading Plasma ports to 5.7.2 (WIP)
    Committed from host : Bola

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -90,9 +90,9 @@ if {![ info exists kf5.release ]} {
 
 # KF5 Plasma version
 if {![ info exists kf5.plasma ]} {
-    set kf5.plasma      5.6.4
+    set kf5.plasma      5.7.2
     set kf5.latest_plasma \
-                        5.6.5
+                        5.7.2
 }
 
 platforms               darwin linux
