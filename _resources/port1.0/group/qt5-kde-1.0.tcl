391cdfa996aad94858e5b769d2489b2fe9ac78e0
René J.V. Bertin <rjvbertin@gmail.com>
2016-10-05 14:09:14 +0200

    Qt5-kde PortGroup : drop the de-facto dependency on qt5-kde-qtwebkit. Dependent ports now have to use either
    depends_lib-append ${qt5webkit_dependency}
    
    or
    
    qt5.depends_component qtwebkit
    
    Committed from host : Portia.local

--- _resources/port1.0/group/qt5-kde-1.0.tcl
+++ _resources/port1.0/group/qt5-kde-1.0.tcl
@@ -318,7 +318,7 @@ if {![info exists building_qt5]} {
             set qt5_pathlibspec path:libexec/${qt_name}/lib/libQtWebKit.${qt_libs_ext}
         }
         set qt5webkit_dependency ${qt5_pathlibspec}:qt5-kde-qtwebkit
-        depends_lib-append ${qt5_dependency} ${qt5webkit_dependency}
+        depends_lib-append ${qt5_dependency}
         if {[info exists qt5.depends_qtwebengine] && ${qt5.depends_qtwebengine}} {
             depends_lib-append \
                 path:libexec/${qt_name}/Library/Frameworks/QtWebEngineCore.framework/QtWebEngineCore:qt5-kde-qtwebengine
@@ -327,7 +327,7 @@ if {![info exists building_qt5]} {
         set qt5_pathlibspec path:libexec/${qt_name}/lib/libQt5Core.${qt_libs_ext}
         set qt5_dependency ${qt5_pathlibspec}:qt5-kde
         set qt5webkit_dependency path:libexec/${qt_name}/lib/libQt5WebKit.${qt_libs_ext}:qt5-kde-qtwebkit
-        depends_lib-append ${qt5_dependency} ${qt5webkit_dependency}
+        depends_lib-append ${qt5_dependency}
         if {[info exists qt5.depends_qtwebengine] && ${qt5.depends_qtwebengine}} {
             depends_lib-append \
                 path:libexec/${qt_name}/lib/libQt5WebEngineCore.${qt_libs_ext}:qt5-kde-qtwebengine
