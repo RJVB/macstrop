130349ebbd50d6cda305e748c272ab374c297b5e
René J.V. Bertin <rjvbertin@gmail.com>
2016-10-12 19:04:32 +0200

    preserve_runtime_libraries PortGroup : add (C) notice
    Committed from host : Portia.local

--- _resources/port1.0/group/preserve_runtime_libraries-1.0.tcl
+++ _resources/port1.0/group/preserve_runtime_libraries-1.0.tcl
@@ -2,6 +2,7 @@
 # $Id:$
 
 # Copyright (c) 2015 The MacPorts Project
+# Copyright (c) 2016 R.J.V. Bertin
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
