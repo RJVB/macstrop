66975b971edad05c14656313075b28a809e5d8c6
R.J.V. Bertin <rjvbertin@gmail.com>
2016-12-14 18:00:37 +0100

    qt5-kde PG : qt5.depends_component avoids adding stub ports
    Committed from host : Portia.local

--- _resources/port1.0/group/qt5-1.0.tcl
+++ _resources/port1.0/group/qt5-1.0.tcl
@@ -103,7 +103,9 @@ proc qt5.depends_component {first args} {
     # join ${first} and (the optional) ${args}
     set args [linsert $args[set list {}] 0 ${first}]
     # select the Qt5 port prefix, depending on which Qt5 port is installed
-    if {([info exists qt5.using_kde] && ${qt5.using_kde}) || ${os.major} == 10} {
+    set is_qt5kde [expr [info exists qt5.using_kde] && ${qt5.using_kde}]
+    if {${is_qt5kde} == 1 || ${os.major} == 10} {
+        # We have port:qt5-kde or we're on OS X 10.6 which only gets Qt 5.3.2 from that port
         set qt5pprefix  "qt5-kde"
     } elseif {${os.major} == 11} {
         set qt5pprefix  "qt55"
@@ -111,15 +113,31 @@ proc qt5.depends_component {first args} {
         set qt5pprefix  "qt5"
     }
     foreach comp ${args} {
-        if {${comp} eq "qt5"} {
-            if {[info exists qt5.using_kde] && ${qt5.using_kde}} {
-                global qt5_dependency
-                # qt5-kde-1.0.tcl exports the dependency expression in a variable
-                depends_lib-append ${qt5_dependency}
-            } else {
-                depends_lib-append port:${qt5pprefix}
+        set done true
+        switch ${comp} {
+            "qt5" {
+                if {${is_qt5kde} == 1} {
+                    global qt5_dependency
+                    # qt5-kde-1.0.tcl exports the exact dependency expression in a variable
+                    depends_lib-append ${qt5_dependency}
+                } else {
+                    depends_lib-append port:${qt5pprefix}
+                }
             }
-        } else {
+            "qtwebkit" -
+            "qtwebengine" -
+            "qtwebview" {
+                # these components are never stub subports
+                set done false
+            }
+            default {
+                # these components are included port:qt5-kde (and provided as additional stub subports)
+                if {${is_qt5kde} == 0} {
+                    set done false
+                }
+            }
+        }
+        if {!${done}} {
             set portname "${qt5pprefix}-${comp}"
             if {[info exists qt5_component_lib] && [info exists qt5_component_lib(${comp})]} {
                 # an explicit dependency pattern was given, e.g. path:foo
