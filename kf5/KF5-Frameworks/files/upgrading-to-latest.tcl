0fecf6e73f8d253f65933d0a345e3155252880e0
R.J.V. Bertin <rjvbertin@gmail.com>
2016-12-20 12:58:49 +0100

    KF5-Frameworks : hide more of the dirty details around the upgrade procedure
    Committed from host : Portia.local

---
+++ kf5/KF5-Frameworks/files/upgrading-to-latest.tcl
@@ -0,0 +1,20 @@
+if {${kf5.latest_version} ne ${kf5.version}} {
+    kf5.use_latest  kf5.version
+    version         ${kf5.version}
+    set checksums.table \
+                    checksums-${kf5.version}.table
+} else {
+    set checksums.table \
+                    checksums.table
+}
+
+# a convenience procedure that allows to let non-dev users build the
+# previous version which still requires a specific patch that has become
+# obsolete in the new version to which all subports are being upgraded.
+proc versioned_patchfile_append {d f} {
+    global filespath version
+    if {[file exists ${filespath}/${d}/${version}/${f}]} {
+        patchfiles-append \
+                    ${d}/${version}/${f}
+    }
+}
