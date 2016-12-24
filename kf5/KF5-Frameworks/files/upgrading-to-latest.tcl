2331b5f8281db996e0042e4a62540edc49270508
René J.V. Bertin <rjvbertin@gmail.com>
2016-12-24 10:54:11 +0100

    RETHINK the upgrading-to-latest functionality so that it works from registry copies too!!!
    Committed from host : Bola

--- kf5/KF5-Frameworks/files/upgrading-to-latest.tcl
+++ kf5/KF5-Frameworks/files/upgrading-to-latest.tcl
@@ -1,20 +1,20 @@
-if {(${kf5.latest_version} ne ${kf5.version}) && [file exists ${filespath}/enable_latest]} {
-    kf5.use_latest  kf5.version
-    version         ${kf5.version}
-    set checksums.table \
-                    checksums-${kf5.version}.table
-} else {
-    set checksums.table \
-                    checksums.table
-}
+# if {(${kf5.latest_version} ne ${kf5.version}) && [file exists ${filespath}/enable_latest]} {
+#     kf5.use_latest  kf5.version
+#     version         ${kf5.version}
+#     set checksums.table \
+#                     checksums-${kf5.version}.table
+# } else {
+#     set checksums.table \
+#                     checksums.table
+# }
 
-# a convenience procedure that allows non-dev users to build the
-# previous version which still requires a specific patch that has become
-# obsolete in the new version to which all subports are being upgraded.
-proc versioned_patchfile_append {d f} {
-    global filespath version
-    if {[file exists ${filespath}/${d}/${version}/${f}]} {
-        patchfiles-append \
-                    ${d}/${version}/${f}
-    }
-}
+# # a convenience procedure that allows non-dev users to build the
+# # previous version which still requires a specific patch that has become
+# # obsolete in the new version to which all subports are being upgraded.
+# proc versioned_patchfile_append {d f} {
+#     global filespath version
+#     if {[file exists ${filespath}/${d}/${version}/${f}]} {
+#         patchfiles-append \
+#                     ${d}/${version}/${f}
+#     }
+# }
