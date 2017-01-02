673557c9ebbc47d15519ad1fdebe582a58e7b668
R.J.V. Bertin <rjvbertin@gmail.com>
2017-01-02 21:52:35 +0100

    port:qt5-kde-devel : archive script update
    Committed from host : Portia.local

--- aqua/qt5-kde-devel/archive.sh
+++ aqua/qt5-kde-devel/archive.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
 # chdir to the port directory
 cd `dirname $0`
@@ -9,7 +9,9 @@ CATDIR=`basename ${PWD}`
 cd ..
 
 EXTRAFILES="_resources/port1.0/group/qt5*-1.0.tcl _resources/port1.0/group/qmake5-1.0.tcl _resources/port1.0/group/macports_clang_selection-1.0.tcl _resources/port1.0/group/locale_select-1.0.tcl "
+EXCLUDEFILES="--exclude ${CATDIR}/${PORTDIR}/files/old --exclude ${CATDIR}/${PORTDIR}/files/qt551 --exclude ${CATDIR}/${PORTDIR}/.DS_Store"
 
 # echo ${PORT} ${CATDIR} ${PORTDIR}
-tar -cvjf ${PORT}.tar.bz2 ${EXTRAFILES} ${CATDIR}/${PORTDIR}
+/opt/local/bin/bsdtar -c ${EXCLUDEFILES} \
+    -vjf ${PORT}.tar.bz2 ${EXTRAFILES} ${CATDIR}/${PORTDIR}
 ll -h ${PORT}.tar.bz2
