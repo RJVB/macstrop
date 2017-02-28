75c5ea033678873c4d94e0bac06b10efcf042a87
R.J.V. Bertin <rjvbertin@gmail.com>
2017-03-01 00:22:51 +0100

    port:freetype-infinality: upgrade to 2017.01.15 ("archfan") port:freetype : upgrade to 2.7.1
    Committed from host : Portia.local

--- print/freetype/files/infinality-settings-explained.sh
+++ print/freetype/files/infinality-settings-explained.sh
@@ -32,7 +32,24 @@
 
 
 
+##################################################################
+# From the Freetype 2.7.x adaptation
+# (see https://github.com/archfan/infinality_bundle/blob/master/01_freetype2-iu/freetype2.sh)
+#
 
+# Subpixel hinting mode can be chosen by setting the right TrueType interpreter
+# version. The available settings are:
+#
+#     truetype:interpreter-version=35  # Classic mode (default in 2.6)
+#     truetype:interpreter-version=38  # (Old!) Infinality mode
+#     truetype:interpreter-version=40  # Minimal mode (default in 2.7)
+#
+# There are more properties that can be set, separated by whitespace. Please
+# refer to the FreeType documentation for details.
+
+# Uncomment and configure below
+#export FREETYPE_PROPERTIES="truetype:interpreter-version=40"
+##################################################################
 
 
 #################################################################
