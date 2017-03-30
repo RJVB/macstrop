ae17f07e9190073f06a141c2ee05d7cee6516f6a
R.J.V. Bertin <rjvbertin@gmail.com>
2017-03-30 18:17:49 +0200

    port:qt5-kde : upgrade to Qt 5.8.0
    Committed from host : Portia.local

--- aqua/qt5-kde/archive.sh
+++ aqua/qt5-kde/archive.sh
@@ -8,7 +8,7 @@ cd ..
 CATDIR=`basename ${PWD}`
 cd ..
 
-EXTRAFILES="_resources/port1.0/group/qt5*-1.0.tcl _resources/port1.0/group/qmake5-1.0.tcl _resources/port1.0/group/macports_clang_selection-1.0.tcl _resources/port1.0/group/locale_select-1.0.tcl "
+EXTRAFILES="_resources/port1.0/group/qt5*-1.0.tcl _resources/port1.0/group/qmake5*-1.0.tcl _resources/port1.0/group/macports_clang_selection-1.0.tcl _resources/port1.0/group/locale_select-1.0.tcl "
 
 # echo ${PORT} ${CATDIR} ${PORTDIR}
 tar -cvjf ${PORT}.tar.bz2 ${EXTRAFILES} ${CATDIR}/${PORTDIR} 
