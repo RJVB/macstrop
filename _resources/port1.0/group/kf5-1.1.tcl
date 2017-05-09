d2f38bb537d6f911c9251b1c2a0d1508141d6091
René J.V. Bertin <rjvbertin@gmail.com>
2017-05-09 17:59:13 +0200

    KF5 PG: allow use in non-KDE ports for generating API documentation
    Committed from host : Bola

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -240,99 +240,6 @@ if {${kf5::includecounter} == 0} {
         configure.args-delete \
                             -DBUILD_doc=OFF \
                             -DBUILD_docs=OFF
-        if {${subport} ne "kf5-kapidox" && ${subport} ne "kf5-kapidox-devel"} {
-            if {![info exists kf5.framework]} {
-                kf5.depends_build_frameworks \
-                            kdoctools
-            }
-            if {[info exists kf5.allow_apidocs_generation] && ${kf5.allow_apidocs_generation}} {
-                if {[info exists kf5.framework]} {
-                    # KF5 frameworks are more or less guaranteed to have a metainfo.yaml file
-                    # which is required for newer kapidox versions. We could check for
-                    # the existence of that file, but that would mean depending on both
-                    # versions of the framework.
-                    kf5.depends_build_frameworks \
-                            kapidox
-                } else {
-                    # other software will be processed by an older KApiDox version,
-                    # installed under the name kf5-kgenapidox
-                    kf5.depends_build_frameworks \
-                            kgenapidox
-                }
-                post-build {
-                    ui_msg "--->  Generating documentation for ${subport} (this can take a while)"
-                    # generate the documentation, working from ${build.dir}
-                    file delete -force ${workpath}/apidocs
-                    xinstall -m 755 -d ${workpath}/apidocs
-                    if {[variant_exists chm] && [variant_isset chm]} {
-                        set chmargs "--chm --chmcompiler ${prefix}/bin/chmcmd"
-                    } else {
-                        set chmargs ""
-                    }
-                    # this appears to be necessary, sometimes:
-                    system "chmod 755 ${workpath}/apidocs"
-                    if {[info exists kf5.framework]} {
-                        if {[catch {system -W ${build.dir} "kapidox_generate --qhp --searchengine --api-searchbox --indexing \
-                            ${chmargs} \
-                            --qtdoc-dir ${qt_docs_dir} --qhelpgenerator ${qt_bins_dir}/qhelpgenerator \
-                            ${worksrcpath}"} result context]} {
-                            ui_msg "Failure generating documentation: ${result}"
-                        }
-                        # after creating the destination, copy all generated qch documentation to it
-                        foreach doc [glob -nocomplain ${build.dir}/frameworks/*/qch/*.qch \
-                                ${build.dir}/*/qch/*.qch ${build.dir}/*/html/*.chm] {
-                            if {[file tail ${doc}] ne "None.qch"} {
-                                system "chmod 644 ${doc}"
-                                xinstall -m 644 ${doc} ${workpath}/apidocs/
-                            }
-                        }
-                        file delete -force ${build.dir}/${kf5.framework}-${version}
-                    } else {
-                        system -W ${build.dir} "kgenapidox --qhp --searchengine --api-searchbox \
-                            ${chmargs} \
-                            --qtdoc-dir ${qt_docs_dir} --kdedoc-dir ${kf5.docs_dir} \
-                            --qhelpgenerator ${qt_bins_dir}/qhelpgenerator ${worksrcpath}"
-                        # after creating the destination, copy all generated qch documentation to it
-                        foreach doc [glob -nocomplain ${build.dir}/apidocs/qch/*.qch  ${build.dir}/apidocs/html/*.chm] {
-                            if {[file tail ${doc}] ne "None.qch"} {
-                                system "chmod 644 ${doc}"
-                                xinstall -m 644 ${doc} ${workpath}/apidocs/
-                            }
-                        }
-                        file delete -force ${build.dir}/apidocs
-                    }
-                }
-                post-destroot {
-                    xinstall -m 755 -d ${destroot}${kf5.docs_dir}
-                    # this appears to be necessary, sometimes:
-                    system "chmod 755 ${destroot}${kf5.docs_dir}"
-                    foreach doc [glob -nocomplain ${workpath}/apidocs/*.qch] {
-                        xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
-                        # maybe not such a useful idea:
-#                         if {[info procs qt5.register_qch_files] ne ""} {
-#                             qt5.register_qch_files ${kf5.docs_dir}/[file tail ${doc}]
-#                         }
-                    }
-                    if {[variant_exists chm] && [variant_isset chm]} {
-                        foreach doc [glob -nocomplain ${workpath}/apidocs/*.chm] {
-                            xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
-                        }
-                    }
-                }
-            }
-        }
-    }
-    if {![variant_isset docs]} {
-        # 20160325 : do this in the post-patch so patches around the targeted won't need to be
-        # specific for +docs and -docs !
-        post-patch {
-            if {[file exists ${worksrcpath}/CMakeLists.txt]} {
-                reinplace "/add_subdirectory.*(\[ ]*docs\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
-                reinplace "/add_subdirectory.*(\[ \]*doc\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
-                reinplace "/ADD_SUBDIRECTORY.*(\[ ]*docs\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
-                reinplace "/ADD_SUBDIRECTORY.*(\[ \]*doc\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
-            }
-        }
     }
     platform darwin {
         post-destroot {
@@ -587,6 +494,101 @@ if {[string first "registry/portgroup" ${kf5::currentportgroupdir}] < 0} {
 if {${kf5::includecounter} == 0} {
     PortGroup kf5-frameworks 1.0
     # TODO: raise error if inclusion failed
+
+    if {[variant_isset docs]} {
+        if {${subport} ne "kf5-kapidox" && ${subport} ne "kf5-kapidox-devel"} {
+            if {![info exists kf5.framework]} {
+                kf5.depends_build_frameworks \
+                            kdoctools
+            }
+            if {[info exists kf5.allow_apidocs_generation] && ${kf5.allow_apidocs_generation}} {
+                if {[info exists kf5.framework]} {
+                    # KF5 frameworks are more or less guaranteed to have a metainfo.yaml file
+                    # which is required for newer kapidox versions. We could check for
+                    # the existence of that file, but that would mean depending on both
+                    # versions of the framework.
+                    kf5.depends_build_frameworks \
+                            kapidox
+                } else {
+                    # other software will be processed by an older KApiDox version,
+                    # installed under the name kf5-kgenapidox
+                    kf5.depends_build_frameworks \
+                            kgenapidox
+                }
+                post-build {
+                    ui_msg "--->  Generating documentation for ${subport} (this can take a while)"
+                    # generate the documentation, working from ${build.dir}
+                    file delete -force ${workpath}/apidocs
+                    xinstall -m 755 -d ${workpath}/apidocs
+                    if {[variant_exists chm] && [variant_isset chm]} {
+                        set chmargs "--chm --chmcompiler ${prefix}/bin/chmcmd"
+                    } else {
+                        set chmargs ""
+                    }
+                    # this appears to be necessary, sometimes:
+                    system "chmod 755 ${workpath}/apidocs"
+                    if {[info exists kf5.framework]} {
+                        if {[catch {system -W ${build.dir} "kapidox_generate --qhp --searchengine --api-searchbox --indexing \
+                            ${chmargs} \
+                            --qtdoc-dir ${qt_docs_dir} --qhelpgenerator ${qt_bins_dir}/qhelpgenerator \
+                            ${worksrcpath}"} result context]} {
+                            ui_msg "Failure generating documentation: ${result}"
+                        }
+                        # after creating the destination, copy all generated qch documentation to it
+                        foreach doc [glob -nocomplain ${build.dir}/frameworks/*/qch/*.qch \
+                                ${build.dir}/*/qch/*.qch ${build.dir}/*/html/*.chm] {
+                            if {[file tail ${doc}] ne "None.qch"} {
+                                system "chmod 644 ${doc}"
+                                xinstall -m 644 ${doc} ${workpath}/apidocs/
+                            }
+                        }
+                        file delete -force ${build.dir}/${kf5.framework}-${version}
+                    } else {
+                        system -W ${build.dir} "kgenapidox --qhp --searchengine --api-searchbox \
+                            ${chmargs} \
+                            --qtdoc-dir ${qt_docs_dir} --kdedoc-dir ${kf5.docs_dir} \
+                            --qhelpgenerator ${qt_bins_dir}/qhelpgenerator ${worksrcpath}"
+                        # after creating the destination, copy all generated qch documentation to it
+                        foreach doc [glob -nocomplain ${build.dir}/apidocs/qch/*.qch  ${build.dir}/apidocs/html/*.chm] {
+                            if {[file tail ${doc}] ne "None.qch"} {
+                                system "chmod 644 ${doc}"
+                                xinstall -m 644 ${doc} ${workpath}/apidocs/
+                            }
+                        }
+                        file delete -force ${build.dir}/apidocs
+                    }
+                }
+                post-destroot {
+                    xinstall -m 755 -d ${destroot}${kf5.docs_dir}
+                    # this appears to be necessary, sometimes:
+                    system "chmod 755 ${destroot}${kf5.docs_dir}"
+                    foreach doc [glob -nocomplain ${workpath}/apidocs/*.qch] {
+                        xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
+                        # maybe not such a useful idea:
+#                         if {[info procs qt5.register_qch_files] ne ""} {
+#                             qt5.register_qch_files ${kf5.docs_dir}/[file tail ${doc}]
+#                         }
+                    }
+                    if {[variant_exists chm] && [variant_isset chm]} {
+                        foreach doc [glob -nocomplain ${workpath}/apidocs/*.chm] {
+                            xinstall -m 644 ${doc} ${destroot}${kf5.docs_dir}
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        # 20160325 : do this in the post-patch so patches around the targeted won't need to be
+        # specific for +docs and -docs !
+        post-patch {
+            if {[file exists ${worksrcpath}/CMakeLists.txt]} {
+                reinplace "/add_subdirectory.*(\[ ]*docs\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
+                reinplace "/add_subdirectory.*(\[ \]*doc\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
+                reinplace "/ADD_SUBDIRECTORY.*(\[ ]*docs\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
+                reinplace "/ADD_SUBDIRECTORY.*(\[ \]*doc\[ \]*)/d" ${worksrcpath}/CMakeLists.txt
+            }
+        }
+    }
 
     proc kf5.add_test_library_path {path} {
         global os.platform
