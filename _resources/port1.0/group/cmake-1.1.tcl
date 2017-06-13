b94e3407373e71c464a24eb817209b17cd781ba6
René J.V. Bertin <rjvbertin@gmail.com>
2017-06-13 21:35:54 +0200

    cmake-1.1 PG : cmake.save_configure_cmd can *optionally* save cmake's output
    Committed from host : Bola

--- _resources/port1.0/group/cmake-1.1.tcl
+++ _resources/port1.0/group/cmake-1.1.tcl
@@ -320,10 +320,12 @@ post-configure {
     }
 }
 
-proc cmake.save_configure_cmd {} {
-    pre-configure {
-        configure.post_args-append "| tee ${workpath}/.macports.${subport}.configure.log"
-        ui_debug "cmake.save_configure_cmd set configure.post_args to \"${configure.post_args}\""
+proc cmake.save_configure_cmd {{save_log_too ""}} {
+    if {${save_log_too} ne ""} {
+        pre-configure {
+            configure.post_args-append "| tee ${workpath}/.macports.${subport}.configure.log"
+            ui_debug "cmake.save_configure_cmd set configure.post_args to \"${configure.post_args}\""
+        }
     }
     post-configure {
         if {![catch {set fd [open "${workpath}/.macports.${subport}.configure.cmd" "w"]} err]} {
@@ -359,16 +361,6 @@ proc cmake.save_configure_cmd {} {
         if {[file exists ${build.dir}/CMakeCache.txt]} {
             # keep a backup of the CMake cache file
             file copy -force ${build.dir}/CMakeCache.txt ${build.dir}/CMakeCache-MacPorts.txt
-        }
-        set portdir [getportdir "file://${filespath}/.."]
-        set logfile [file join [getportlogpath ${portdir} ${subport}] "main.log"]
-        if {[file exists ${logfile}]} {
-            catch {
-                # force a flush by appending to the file via an external command
-                system "echo '\nbacking up logfile' >> ${logfile}"
-                system "sync"
-                file copy -force ${logfile} ${workpath}/.macports.${subport}.configure.log2
-            }
         }
     }
 }
