8be885f544865498d1791c65057a9cd7c6eb72b9
R.J.V. Bertin <rjvbertin@gmail.com>
2017-06-15 18:17:54 +0200

    KF5 PG : latest Applications is 17.04.2
    Committed from host : Portia.local

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -116,7 +116,7 @@ if {![info exists kf5.version]} {
 if {![ info exists kf5.release ]} {
     set kf5.release     16.12.0
     set kf5.latest_release \
-                        17.04.0
+                        17.04.2
 }
 
 # KF5 Plasma version
