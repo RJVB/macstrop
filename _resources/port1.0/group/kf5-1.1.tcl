23a13915a332e5d5635dfd0583abb1c627a45681
René J.V. Bertin <rjvbertin@gmail.com>
2017-08-04 09:37:39 +0200

    KF5 PG : remove $id line
    Committed from host : Bola

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -1,5 +1,4 @@
 # -*- coding: utf-8; mode: tcl; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; truncate-lines: t -*- vim:fenc=utf-8:et:sw=4:ts=4:sts=4
-# $Id: kf5-1.1.tcl 134210 2015-03-20 06:40:18Z mk@macports.org $
 
 # Copyright (c) 2015 The MacPorts Project
 # Copyright (c) 2015, 2016 R.J.V. Bertin
