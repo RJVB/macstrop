0368b80d6d46463c48c7f4a97dd77632064e887c
R.J.V. Bertin <rjvbertin@gmail.com>
2017-08-06 18:14:29 +0200

    port:qt53-kde* : initial untested commit
    Committed from host : Portia.local

---
+++ aqua/qt53-kde/archive.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# chdir to the port directory
+cd `dirname $0`
+PORT=`port -q info --name`
+PORTDIR=`basename ${PWD}`
+cd ..
+CATDIR=`basename ${PWD}`
+cd ..
+
+EXTRAFILES="_resources/port1.0/group/qt5*-1.0.tcl _resources/port1.0/group/qmake5*-1.0.tcl _resources/port1.0/group/macports_clang_selection-1.0.tcl _resources/port1.0/group/locale_select-1.0.tcl "
+
+# echo ${PORT} ${CATDIR} ${PORTDIR}
+tar -cvjf ${PORT}.tar.bz2 ${EXTRAFILES} ${CATDIR}/${PORTDIR} 
+ll -h ${PORT}.tar.bz2
