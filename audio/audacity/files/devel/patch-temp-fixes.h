06db0c993bcf34b755831cdcca53cfa1ddfafaaf
René J.V. Bertin <rjvbertin@gmail.com>
2017-10-19 16:13:33 +0200

    port:audacity-devel : upgrade (wip, linux)
    Committed from host : Bola
    
    Committed from host : Bola
    
    Committed from host : Bola

--- audio/audacity/files/devel/patch-temp-fixes.h
+++ audio/audacity/files/devel/patch-temp-fixes.h
@@ -10,16 +10,3 @@ index 1eb296e307141305018799636edd9ad1fee89159..df809bc79c08aff4a2930d88c021f09e
  
  #ifndef safenew
  #define safenew new
-diff --git a/src/Mix.cpp b/src/Mix.cpp
-index a16ca05e25f3095230b0c507293a98493eb0a262..7abf44da3cb39b301b50927494693957471af12e 100644
---- a/src/Mix.cpp
-+++ b/src/Mix.cpp
-@@ -255,7 +255,7 @@ Mixer::Mixer(const WaveTrackConstArray &inputTracks,
-    , mQueueMaxLen{ 65536 }
-    , mSampleQueue{ mNumInputTracks, mQueueMaxLen }
- 
--   , mNumChannels{ static_cast<size_t>(numOutChannels) }
-+   , mNumChannels{ static_cast<unsigned>(numOutChannels) }
-    , mGains{ mNumChannels }
- 
-    , mMayThrow{ mayThrow }
