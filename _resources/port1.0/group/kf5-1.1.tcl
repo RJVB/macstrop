2227cfc176a5a8fd3f7d6318bf581fc05330e5f1
R.J.V. Bertin <rjvbertin@gmail.com>
2018-11-19 21:56:08 +0100

    KF5 PG : no need to disable find_package(X11) on Mac
    the ECM already take care of this
    
    Committed from host : Portia.local

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -206,7 +206,6 @@ if {${kf5::includecounter} == 0} {
                             ${prefix}/lib/pkgconfig
         configure.args-append \
                             -DBUNDLE_INSTALL_DIR=${kf5.applications_dir} \
-                            -DCMAKE_DISABLE_FIND_PACKAGE_X11=ON \
                             -DAPPLE_SUPPRESS_X11_WARNING=ON \
                             -DCMAKE_INSTALL_LIBEXECDIR=${prefix}/libexec \
                             -DKDE_INSTALL_LIBEXECDIR=${kf5.libexec_dir} \
