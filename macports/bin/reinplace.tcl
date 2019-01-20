5ebb2ce2a45f30344064eb6325cbb051c9859cf1
René J.V. Bertin <rjvbertin@gmail.com>
2019-01-20 20:34:26 +0100

    reinplace.tcl : merge current version from "base", refactor to add -v argument
    Committed from host : Bola

--- macports/bin/reinplace.tcl
+++ macports/bin/reinplace.tcl
@@ -19,31 +19,21 @@ package require Tclx
 package require macports 1.0
 package require Pextlib 1.0
 
-mportinit
-
-#set os_platform [string tolower $tcl_platform(os)]
-set os_platform ${macports::os_platform}
-set macosx_version ${macports::macosx_version}
-set os_version ${macports::os_version}
-set os_arch ${macports::os_arch}
-set portpath "./"
-set portbuildpath "./"
-
-package require port 1.0
+array set ui_options        {}
+array set global_options    {}
+array set global_variations {}
 
 # reinplace
 # Provides "sed in place" functionality
 set args $argv
-#proc reinplace {args}  {
-    puts "$args"
-    global env macosx_version
+
+# proc reinplace {args}  {
+    global env workpath macosx_version ui_options
     set extended 0
     set suppress 0
-    # once a macports version has been released, add the rest of the
-    # code from https://trac.macports.org/ticket/15514
     set quiet 0
     set oldlocale_exists 0
-    set oldlocale "" 
+    set oldlocale ""
     set locale ""
     set dir ""
     while 1 {
@@ -68,6 +58,10 @@ set args $argv
                 q {
                     set quiet 1
                 }
+                v {
+                    puts "$args"
+                    set ui_options(ports_verbose) yes
+                }
                 W {
                     set dir [lindex $args 0]
                     set args [lrange $args 1 end]
@@ -89,7 +83,23 @@ set args $argv
     set pattern [lindex $args 0]
     set files [lrange $args 1 end]
 
-    set tempdir /tmp
+    ### init
+    mportinit ui_options global_options global_variations
+    #set os_platform [string tolower $tcl_platform(os)]
+    set os_platform ${macports::os_platform}
+    set macosx_version ${macports::macosx_version}
+    set os_version ${macports::os_version}
+    set os_arch ${macports::os_arch}
+    set portpath "./"
+    set portbuildpath "./"
+    package require port 1.0
+    ### init done
+
+#     if {[file isdirectory ${workpath}/.tmp]} {
+#         set tempdir ${workpath}/.tmp
+#     } else {
+        set tempdir /tmp
+#     }
 
     foreach file $files {
         global UI_PREFIX
@@ -99,9 +109,8 @@ set args $argv
         set file [file join $dir $file]
 
         if {[catch {set tmpfile [mkstemp "${tempdir}/[file tail $file].sed.XXXXXXXX"]} error]} {
-            global errorInfo
-            puts "$errorInfo"
-            puts "reinplace: $error"
+            ui_debug $::errorInfo
+            ui_error "reinplace: $error"
             return -code error "reinplace failed"
         } else {
             # Extract the Tcl Channel number
@@ -113,11 +122,7 @@ set args $argv
         set cmdline {}
         lappend cmdline $portutil::autoconf::sed_command
         if {$extended} {
-            if {$portutil::autoconf::sed_ext_flag eq "N/A"} {
-                puts "sed extended regexp not available"
-                return -code error "reinplace sed(1) too old"
-            }
-            lappend cmdline $portutil::autoconf::sed_ext_flag
+            lappend cmdline -E
         }
         if {$suppress} {
             lappend cmdline -n
@@ -126,12 +131,11 @@ set args $argv
         if {$locale ne ""} {
             set env(LC_CTYPE) $locale
         }
-        puts "$UI_PREFIX [format [msgcat::mc "Patching %s: %s"] [file tail $file] $pattern]"
-        puts "Executing reinplace: $cmdline"
+        ui_info "$UI_PREFIX [format [msgcat::mc "Patching %s: %s"] [file tail $file] $pattern]"
+        ui_debug "Executing reinplace: $cmdline"
         if {[catch {exec -ignorestderr -- {*}$cmdline} error]} {
-            global errorInfo
-            puts "$errorInfo"
-            puts "reinplace: $error"
+            ui_debug $::errorInfo
+            ui_error "reinplace: $error"
             file delete "$tmpfile"
             if {$locale ne ""} {
                 if {$oldlocale_exists} {
@@ -153,24 +157,24 @@ set args $argv
         }
         close $tmpfd
 
+        if {!$quiet && ![catch {exec -ignorestderr cmp -s $file $tmpfile}]} {
+            ui_warn "[format [msgcat::mc "reinplace %1\$s didn't change anything in %2\$s"] $pattern $file]"
+        }
+
         set attributes [file attributes $file]
-        # start gsoc08-privileges
         chownAsRoot $file
-        # end gsoc08-privileges
 
         # We need to overwrite this file
         if {[catch {file attributes $file -permissions u+w} error]} {
-            global errorInfo
-            puts "$errorInfo"
-            puts "reinplace: $error"
+            ui_debug $::errorInfo
+            ui_error "reinplace: $error"
             file delete "$tmpfile"
             return -code error "reinplace permissions failed"
         }
 
         if {[catch {file copy -force $tmpfile $file} error]} {
-            global errorInfo
-            puts "$errorInfo"
-            puts "reinplace: $error"
+            ui_debug $::errorInfo
+            ui_error "reinplace: $error"
             file delete "$tmpfile"
             return -code error "reinplace copy failed"
         }
@@ -180,7 +184,7 @@ set args $argv
         file delete "$tmpfile"
     }
     return
-#}
+# }
 
 #reinplace [lindex $argv 0] [lindex $argv 1]
 #reinplace $argv
