b57f2e2c4c50440c52d3b1a4ca684cf35580fa3f
René J.V. Bertin <rjvbertin@gmail.com>
2019-02-10 12:21:31 +0100

    meson PG : add meson.save_configure_cmd procedure
    Committed from host : Bola

--- _resources/port1.0/group/meson-1.0.tcl
+++ _resources/port1.0/group/meson-1.0.tcl
@@ -66,3 +66,58 @@ pre-configure {
     configure.pre_args-append \
                             --buildtype=custom
 }
+
+proc meson.save_configure_cmd {{save_log_too ""}} {
+    if {${save_log_too} ne ""} {
+        pre-configure {
+            configure.pre_args-prepend "-cf '${configure.cmd} "
+            configure.post_args-append "|& tee ${workpath}/.macports.${subport}.configure.log'"
+            configure.cmd "/bin/csh"
+            ui_debug "configure command set to `${configure.cmd} ${configure.pre_args} ${configure.args} ${configure.post_args}`"
+        }
+    }
+    post-configure {
+        if {![catch {set fd [open "${workpath}/.macports.${subport}.configure.cmd" "w"]} err]} {
+            foreach var [array names ::env] {
+                puts ${fd} "${var}=$::env(${var})"
+            }
+            puts ${fd} "[join [lrange [split ${configure.env} " "] 0 end] "\n"]"
+            # the following variables are no longer set in the environment at this point:
+            puts ${fd} "CPP=\"${configure.cpp}\""
+            # these are particularly relevant because referenced in the configure.pre_args:
+            puts ${fd} "CC=\"${configure.cc}\""
+            puts ${fd} "CXX=\"${configure.cxx}\""
+            if {${configure.objcxx} ne ${configure.cxx}} {
+                puts ${fd} "OBJCXX=\"${configure.objcxx}\""
+            }
+            puts ${fd} "CFLAGS=\"${configure.cflags}\""
+            puts ${fd} "CXXFLAGS=\"${configure.cxxflags}\""
+            if {${configure.objcflags} ne ${configure.cflags}} {
+                puts ${fd} "OBJCFLAGS=\"${configure.objcflags}\""
+            }
+            if {${configure.objcxxflags} ne ${configure.cxxflags}} {
+                puts ${fd} "OBJCXXFLAGS=\"${configure.objcxxflags}\""
+            }
+            puts ${fd} "LDFLAGS=\"${configure.ldflags}\""
+            puts ${fd} "# Commandline configure options:"
+            if {${configure.optflags} ne ""} {
+                puts -nonewline ${fd} " configure.optflags=\"${configure.optflags}\""
+            }
+            if {${configure.compiler} ne ""} {
+                puts -nonewline ${fd} " configure.compiler=\"${configure.compiler}\""
+            }
+            if {${configure.cxx_stdlib} ne ""} {
+                puts -nonewline ${fd} " configure.cxx_stdlib=\"${configure.cxx_stdlib}\""
+            }
+            if {${configureccache} ne ""} {
+                puts -nonewline ${fd} " configureccache=\"${configureccache}\""
+            }
+            puts ${fd} ""
+            puts ${fd} "\ncd ${worksrcpath}"
+            puts ${fd} "${configure.cmd} [join ${configure.pre_args}] [join ${configure.args}] [join ${configure.post_args}]"
+            close ${fd}
+            unset fd
+        }
+    }
+}
+
