cf84ffe730c6dbfa07cab24ffb40e19785d48dbd
René Bertin <rjvbertin@gmail.com>
2019-07-07 13:31:12 +0200

    LTO PG : provide a means to override ThinLTO
    Committed from host : Bola

--- _resources/port1.0/group/LTO-1.0.tcl
+++ _resources/port1.0/group/LTO-1.0.tcl
@@ -69,8 +69,8 @@ if {[variant_isset LTO] && ![info exists building_qt5]} {
 #             }
             # detect support for flto=thin but only with MacPorts clang versions (being a bit cheap here)
             set clang_version [string map {"macports-clang-" ""} ${configure.compiler}]
-            if {"${clang_version}" ne "${configure.compiler}" && [vercmp ${clang_version} "4.0"] >= 0} {
-                # the compiler supports "ThinLTO", use it
+            if {[tbool LTO.allow_ThinLTO] && "${clang_version}" ne "${configure.compiler}" && [vercmp ${clang_version} "4.0"] >= 0} {
+                # the compiler supports "ThinLTO", use it if allowed
                 set lto_flags           "-flto=thin"
             } else {
                 set lto_flags           "-flto"
