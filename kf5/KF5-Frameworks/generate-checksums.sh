9c5123157d76faa1d143297387afe3f5f998a26f
R.J.V. Bertin <rjvbertin@gmail.com>
2019-07-15 20:07:56 +0200

    port:KF5 Frameworks : upgrading to 5.60.0 (Tier1 done)
    Committed from host : Portia.local

--- kf5/KF5-Frameworks/generate-checksums.sh
+++ kf5/KF5-Frameworks/generate-checksums.sh
@@ -5,30 +5,44 @@ if [ "${prefix}" = "" ] ;then
 fi
 export PATH="${prefix}/bin:${PATH}"
 
-version=5.52.0
+version=5.60.0
 branch=${version%.0}
 
 METAPORT="KF5-Frameworks"
 PORTFILE=`port file ${METAPORT}`
 FRAMEWORKS="`grep '^[   ]*subport ' ${PORTFILE} | sed -e 's|subport \(.*\) {|\1|g'`"
+# # use the set_kf5_framework command which give us the actual remove filenames but leaves us to guess the portname
+# FRAMEWORKS="`sed -e ':a' -e '/^#/d; /\\\\$/N; s|\\\\\n||g; ta' < ${PORTFILE} \
+#     | fgrep 'set_kf5_framework' \
+#     | fgrep -v 'proc set_kf5_framework' \
+#     | sed -e 's|set_kf5_framework[ \t][ \t]*\(.*\)|\1|g'`"
 # echo "Framework list: ${FRAMEWORKS}" 1>&2
 MASTERSITE="https://download.kde.org/stable/frameworks/${branch}"
 MASTERSITEPA="${MASTERSITE}/portingAids"
 
-echo "# checksums for KF5 Frameworks ${version}"
+echo "# checksums for KF5 Frameworks ${version}" 
 echo "\narray set checksumtable {"
 
 DISTDIR="${prefix}/var/macports/distfiles/${METAPORT}"
 mkdir -p "${DISTDIR}"
 
 for F in ${FRAMEWORKS} ;do
-    # remove the "kf5-" prefix
+    # remove the "kf5-" prefix (which shouldn't be there anymore)
     FW="${F#kf5-}"
-    if [ "${FW}" = "ksyntaxhighlighting" ] ;then
-        FW="syntax-highlighting"
-    fi
+    case ${FW} in
+        ksyntaxhighlighting)
+            FW="syntax-highlighting"
+            ;;
+        bluezqt)
+            FW="bluez-qt"
+            ;;
+        networkmanagerqt)
+            FW="networkmanager-qt"
+            ;;
+    esac
     DISTFILE="${DISTDIR}/${FW}-${version}.tar.xz"
     DISTNAME="${MASTERSITE}/${FW}-${version}.tar.xz"
+#     echo "${F} -> ${DISTNAME} -> ${DISTFILE}" 1>&2
     if [ ! -e "${DISTFILE}" ] ;then
         wget -P "${DISTDIR}" "${DISTNAME}"
         if [ $? != 0 ] ;then
