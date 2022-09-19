7e6123bc30a08e3ddd90299f534e2024f4b0b66d
René J.V. Bertin <rjvbertin@gmail.com>
2022-09-19 18:57:30 +0200

    port:macports-libcxx : new upgraded from mainstream port:libcxx : use the libc++ from macports-libc++ if available.
    Committed from host : Portia.local

--- lang/libcxx/files/optlibs.sh
+++ lang/libcxx/files/optlibs.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-if [ "${DYLD_INSERT_LIBRARIES}" != "" ] ;then
-	DYLD_INSERT_LIBRARIES="${DYLD_INSERT_LIBRARIES}:@PREFIX@/lib/libc++.1.dylib"
+if [ -x @PREFIX@/lib/libcxx/libc++.1.dylib ] ;then
+	LIBCXXPATH=@PREFIX@/lib/libcxx
 else
-	DYLD_INSERT_LIBRARIES="@PREFIX@/lib/libc++.1.dylib"
+	LIBCXXPATH=@PREFIX@/lib
+fi
+
+if [ "${DYLD_INSERT_LIBRARIES}" != "" ] ;then
+	DYLD_INSERT_LIBRARIES="${DYLD_INSERT_LIBRARIES}:${LIBCXXPATH}/libc++abi.1.dylib:${LIBCXXPATH}/libc++.1.dylib"
+else
+	DYLD_INSERT_LIBRARIES="${LIBCXXPATH}/libc++abi.1.dylib:${LIBCXXPATH}/libc++.1.dylib"
 fi
 export DYLD_INSERT_LIBRARIES
 
