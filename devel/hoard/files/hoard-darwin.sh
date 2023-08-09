678f0708981ce92f76a6b6154a75ed3bdb3eb4de
René J.V. Bertin <rjvbertin@gmail.com>
2023-08-10 00:22:08 +0200

    port:hoard : upgrade to 3.13.54
    Committed from host : Portia.local

--- devel/hoard/files/hoard-darwin.sh
+++ devel/hoard/files/hoard-darwin.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-export DYLD_INSERT_LIBRARIES=@PREFIX@/lib/libhoard.dylib:${LD_PRELOAD}
+if [ "${DYLD_INSERT_LIBRARIES}" != "" ] ;then
+    export DYLD_INSERT_LIBRARIES="@PREFIX@/lib/libhoard.dylib:${DYLD_INSERT_LIBRARIES}"
+else
+    export DYLD_INSERT_LIBRARIES="@PREFIX@/lib/libhoard.dylib"
+fi
 exec "$@"
