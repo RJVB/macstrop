d996e024d147dfff0aeba50eeb258d060a7a33dc
René Bertin <rjvbertin@gmail.com>
2024-08-15 15:50:10 +0200

    port-vacuum.tcl : cosmetics
    Committed from host : Bola

--- macports/bin/port-vacuum.tcl
+++ macports/bin/port-vacuum.tcl
@@ -11,14 +11,22 @@ set ui_options(ports_debug) yes
 set ui_options(ports_verbose) yes
 
 ### init
+puts -nonewline "Initialising MacPorts ..."
+flush stdout
 mportinit ui_options global_options global_variations
+puts " done"
 
 ui_msg "MacPorts version [macports::version], registry version [registry::metadata get version]"
 
+set maybe ""
 if {[catch {registry::set_needs_vacuum}]} {
 	ui_warn "Your MacPorts version doesn't allow to program a registry VACUUM operation"
+	set maybe "maybe "
 } else {
 	ui_msg "Called registry::set_needs_vacuum"
 }
 
+puts -nonewline "Closing registry and ${maybe}performing vacuum ..."
+flush stdout
 mportshutdown
+puts " done"
