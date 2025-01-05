2ec5c2714caf221ee2f9524425c93bc64f292dcb
René Bertin <rjvbertin@gmail.com>
2025-01-05 20:50:15 +0100

    KDE4 1.1 PG : fix circular dependency in port:automoc introduced by moving the set-up to a callback
    Also add basic Linux support
    
    Committed from host : Bola

--- _resources/port1.0/group/kde4-1.1.tcl
+++ _resources/port1.0/group/kde4-1.1.tcl
@@ -316,7 +316,7 @@ proc kde4.add_app_wrapper {wrappername {bundlename ""} {bundleexec ""}} {
 
 proc kde4::configure_build {} {
 
-    global prefix applications_dir
+    global prefix applications_dir subport os.platform
     global qt_cmake_defines qt_cmake_module_dir
     global cmake_share_module_dir
     global kde4.cmake_module_dir
@@ -330,8 +330,13 @@ proc kde4::configure_build {} {
     # programs currently need it. The automoc port, which includes this
     # PortGroup overrides depends_build, removing "port:automoc" to
     # prevent a cyclic dependency
-    depends_build-delete    port:automoc
-    depends_build-append    port:automoc
+    if {${subport} ne "automoc"} {
+        ui_debug "kde4-1.1 PG : depends_build-append    port:automoc"
+        depends_build-delete \
+                            port:automoc
+        depends_build-append \
+                            port:automoc
+    }
 
     # Phonon added as library dependency here as most, if not all KDE
     # programs current need it.  The phonon port, which includes this
@@ -362,42 +367,43 @@ proc kde4::configure_build {} {
 
     # explicitly define certain headers and libraries, to avoid
     # conflicts with those installed into system paths by the user.
+    set shext [expr {${os.platform} eq "darwin" ? "dylib" : "so"}]
     configure.args-append   -DDOCBOOKXSL_DIR=${prefix}/share/xsl/docbook-xsl-nons \
                             -DGETTEXT_INCLUDE_DIR=${prefix}/include \
-                            -DGETTEXT_LIBRARY=${prefix}/lib/libgettextlib.dylib \
+                            -DGETTEXT_LIBRARY=${prefix}/lib/libgettextlib.${shext} \
                             -DGIF_INCLUDE_DIR=${prefix}/include \
-                            -DGIF_LIBRARY=${prefix}/lib/libgif.dylib \
+                            -DGIF_LIBRARY=${prefix}/lib/libgif.${shext} \
                             -DJASPER_INCLUDE_DIR=${prefix}/libexec/jasper2/include \
-                            -DJASPER_LIBRARY=${prefix}/libexec/jasper2/lib/libjasper.dylib \
+                            -DJASPER_LIBRARY=${prefix}/libexec/jasper2/lib/libjasper.${shext} \
                             -DJPEG_INCLUDE_DIR=${prefix}/include \
-                            -DJPEG_LIBRARY=${prefix}/lib/libjpeg.dylib \
-                            -DLBER_LIBRARIES=${prefix}/lib/liblber.dylib \
+                            -DJPEG_LIBRARY=${prefix}/lib/libjpeg.${shext} \
+                            -DLBER_LIBRARIES=${prefix}/lib/liblber.${shext} \
                             -DLDAP_INCLUDE_DIR=${prefix}/include \
-                            -DLDAP_LIBRARIES=${prefix}/lib/libldap.dylib \
+                            -DLDAP_LIBRARIES=${prefix}/lib/libldap.${shext} \
                             -DLIBEXSLT_INCLUDE_DIR=${prefix}/include \
-                            -DLIBEXSLT_LIBRARIES=${prefix}/lib/libexslt.dylib \
-                            -DLIBICALSS_LIBRARY=${prefix}/lib/libicalss.dylib \
+                            -DLIBEXSLT_LIBRARIES=${prefix}/lib/libexslt.${shext} \
+                            -DLIBICALSS_LIBRARY=${prefix}/lib/libicalss.${shext} \
                             -DLIBICAL_INCLUDE_DIRS=${prefix}/include \
-                            -DLIBICAL_LIBRARY=${prefix}/lib/libical.dylib \
+                            -DLIBICAL_LIBRARY=${prefix}/lib/libical.${shext} \
                             -DLIBINTL_INCLUDE_DIR=${prefix}/include \
-                            -DLIBINTL_LIBRARY=${prefix}/lib/libintl.dylib \
+                            -DLIBINTL_LIBRARY=${prefix}/lib/libintl.${shext} \
                             -DLIBXML2_INCLUDE_DIR=${prefix}/include/libxml2 \
-                            -DLIBXML2_LIBRARIES=${prefix}/lib/libxml2.dylib \
+                            -DLIBXML2_LIBRARIES=${prefix}/lib/libxml2.${shext} \
                             -DLIBXML2_XMLLINT_EXECUTABLE=${prefix}/bin/xmllint \
                             -DLIBXSLT_INCLUDE_DIR=${prefix}/include \
-                            -DLIBXSLT_LIBRARIES=${prefix}/lib/libxslt.dylib \
+                            -DLIBXSLT_LIBRARIES=${prefix}/lib/libxslt.${shext} \
                             -DOPENAL_INCLUDE_DIR=/System/Library/Frameworks/OpenAL.framework/Headers \
                             -DOPENAL_LIBRARY=/System/Library/Frameworks/OpenAL.framework \
                             -DPNG_INCLUDE_DIR=${prefix}/include \
                             -DPNG_PNG_INCLUDE_DIR=${prefix}/include \
-                            -DPNG_LIBRARY=${prefix}/lib/libpng.dylib \
+                            -DPNG_LIBRARY=${prefix}/lib/libpng.${shext} \
                             -DTIFF_INCLUDE_DIR=${prefix}/include \
-                            -DTIFF_LIBRARY=${prefix}/lib/libtiff.dylib \
+                            -DTIFF_LIBRARY=${prefix}/lib/libtiff.${shext} \
                             -DSOPRANO_PREFIX=${prefix}
 
     # These two can be removed (see #46240):
     #                        -DQCA2_INCLUDE_DIR=${prefix}/include/QtCrypto \
-    #                        -DQCA2_LIBRARIES=${prefix}/lib/libqca.dylib \
+    #                        -DQCA2_LIBRARIES=${prefix}/lib/libqca.${shext} \
     # These ones are obsolete, as purely based on mysql5 paths (see #49296):
     #                        -DMYSQLD_EXECUTABLE=${prefix}/libexec/mysqld \
     #                        -DMYSQL_INCLUDE_DIR=${prefix}/include/mysql5/mysql \
