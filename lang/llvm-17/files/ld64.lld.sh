73eca330eb12edce1256642058c3b14b204886bf
René J.V. Bertin <rjvbertin@gmail.com>
2025-11-05 00:14:33 +0100

    port:lld-17 : fix the wrapper script so it accepts argument strings with embedded spaces.
    Committed from host : Portia.local

--- lang/llvm-17/files/ld64.lld.sh
+++ lang/llvm-17/files/ld64.lld.sh
@@ -2,23 +2,28 @@
 
 LLD=@SUBPREFIX@/bin/wrapped/ld64.lld
 
-ARGS="$@"
 PV=""
 verbose=0
-while [ $# != 0 ] ;do
-	case $1 in
-		-macosx_version_min)
-			PV="-platform_version macos $2 $2"
-			;;
-		-v|--verbose)
-			verbose=1
-			;;
-	esac
-	shift 1
-done
+parse_args () {
+	# scan $@ in a function so we shift a copy and can still pass "${@}" to the actual binary below
+	while [ $# != 0 ] ;do
+		case $1 in
+			-macosx_version_min)
+				PV="-platform_version macos $2 $2"
+				;;
+			-v|--verbose)
+				verbose=1
+				;;
+		esac
+		shift 1
+	done
+}
+parse_args "${@}"
+
 if [ ${verbose} = 1 ] ;then
-	echo "${0}->${LLD} ${ARGS} ${PV}" 1>&2
+	# if all went well, "${@}" is exactly the list of argument passed by our caller
+	echo "${0}->${LLD} "${@}" ${PV}" 1>&2
 fi
-exec ${LLD} ${ARGS} ${PV}
+exec ${LLD} "${@}" ${PV}
 			
 
