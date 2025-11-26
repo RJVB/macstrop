88c87df5cb945caddec6f9458f09bbb7c78b2f24
René J.V. Bertin <rjvbertin@gmail.com>
2025-11-26 15:55:29 +0100

    port:libcxx : merged the +exp17 variant (= upgrade to 17.0.6), with fix for the verbose_abort issue
    Also remembered to account for the updated port:legacy-support
    
    Committed from host : Portia.local

--- lang/libcxx/files/find_reexported_from_legacy-support.sh
+++ lang/libcxx/files/find_reexported_from_legacy-support.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
+exported () {
+	dlsym "$1" ${MPLSSYMBOLS} 2>&1 \
+		| egrep 'libMacportsLegacySupport.a| 0x' | gsed -e '/: symbol not found/d' -e "s/ 0x/${so}&${se}/g"
+}
+
+exported2 () {
+	dlsym "$1" ${MPLSSYMBOLS} 2>&1 \
+		| egrep 'libMacportsLegacySupport.a| 0x' | gsed -e '/: symbol not found/d' \
+			-e "s;^$1::;;g" \
+			-e "s/0x[0-9a-fA-F]* /   /g" \
+			-e 's: (/usr/lib: IGNORE (/usr/lib:g'
+}
+
 if [ $# -ge 1 ] ;then
 	so="`tput smso`"
 	se="`tput rmso`"
 
-	dlsym "$1" `nm --defined-only --just-symbol-name /opt/local/lib/libMacportsLegacySupport.a 2>&1 | sed -e 's/^_//g'` 2>&1 \
-		| egrep 'libMacportsLegacySupport.a| 0x' | sed -e "s/ 0x/${so}&${se}/g"
+	# build our own dlsym copy?
+	MPLSSYMBOLS="`nm --defined-only --just-symbol-name /opt/local/lib/libMacportsLegacySupport.a 2>&1 | sed -e 's/^_//g'`"
+	exported "$1"
+	echo
+	exported2 "$1"
 	echo
 	echo "Check the presence of any of the symbols found in "$1" in `port work libcxx`/llvm*.src/libcxx/lib/libc++unexp.exp"
 	echo " and remember that they need to have an underscore prepended in that file!!"
