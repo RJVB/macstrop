8a06cca6c622ae8af12ba6058789d6affb1d1417
René Bertin <rjvbertin@gmail.com>
2026-02-14 16:49:15 +0100

    port:zeal{,-devel} : fix compatibility with port:qt5-qtbase < 5.13 (+qt5stock_kde) and add bogus --no-sandbox option to port:zeal
    Committed from host : Bola

---
+++ devel/zeal/files/devel/DCMdef.h
@@ -0,0 +1,16 @@
+#ifndef _DCMdef_H_
+
+#ifndef Q_DISABLE_MOVE
+#define Q_DISABLE_MOVE(Class) \
+    Class(Class &&) = delete; \
+    Class &operator=(Class &&) = delete;
+#endif
+
+#ifndef Q_DISABLE_COPY_MOVE
+#define Q_DISABLE_COPY_MOVE(Class) \
+    Q_DISABLE_COPY(Class) \
+    Q_DISABLE_MOVE(Class)
+#endif
+
+#define _DCMdef_H_
+#endif
