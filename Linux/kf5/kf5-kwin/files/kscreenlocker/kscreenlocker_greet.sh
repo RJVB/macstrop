4f4018bf5f6135103395f74bbacb7a017526f988
René Bertin <rjvbertin@gmail.com>
2026-05-20 13:26:56 +0200

    port:kf5-kscreenlocker : improved integration with xscreensaver
    Committed from host : Bmbl

--- Linux/kf5/kf5-kwin/files/kscreenlocker/kscreenlocker_greet.sh
+++ Linux/kf5/kf5-kwin/files/kscreenlocker/kscreenlocker_greet.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-for a in "$@" ;do
-	case ${a} in 
-		--immediateLock)
-			xscreensaver-command -version >/dev/null
-			if [ $? = 0 ] ;then
-				(sleep 5 ; echo "Locked at `date +%s`") &
-				exec xscreensaver-command -lock
-			else
-				if [ -x ${0}.bin ] ;then
-					exec ${0}.bin "$@"
-				else
-					# the KScreenLocker library should unlock gracefully when the greeter crashes
-					kill -ABRT $$
-				fi
-			fi
-			;;
-		*)
-			exec xscreensaver-command -activate
-			;;
-	esac
-done
-exec xscreensaver-command -activate
+xscreensaver-command -version >/dev/null
+if [ $? = 0 ] ;then
+    for a in "$@" ;do
+        case ${a} in 
+            --immediateLock)
+                    (sleep 5 ; echo "Locked at `date +%s`") &
+                    exec xscreensaver-command -lock
+                ;;
+            *)
+                exec xscreensaver-command -activate
+                ;;
+        esac
+    done
+    exec xscreensaver-command -activate
+else
+    if [ -x ${0}.bin ] ;then
+        exec ${0}.bin "$@"
+    else
+        # the KScreenLocker library should unlock gracefully when the greeter crashes
+        kill -ABRT $$
+    fi
+fi
